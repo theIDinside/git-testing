fca303f6f008d7852dfc01ca77b41025cfb4600e
Simon Farre <simon.farre.cx@gmail.com>
2022-01-29 13:43:58 +0100

    collect argv
    Changed log msg
    
    read user input
    
    actually reading user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+fn one_two_or_three(s: &str) -> Option<u8> {
+    match &s[..] {
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        _ => None
+    }
+}
+
 fn main() {
-    println!("Squashing commits, hello!");
+    let args: Vec<String> = std::env::args().collect();
+    let known_value = args.get(1).and_then(|input| one_two_or_three(&input));
+    if let Some(v) = known_value {
+        println!("we know this word! It's value is: {}", v);
+    } else {
+        println!("we did not understand or get input");
+    }   
+    println!("Interactive rebase, hello!");
 }
